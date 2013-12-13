62ce51a320f95cf328032f05f0ece1e4e7b0e711
hulagu <serkan.tas@likyateknoloji.com>
2013-12-13 15:57:18 +0200

    Temp config handler modifed to load global states from xml.

--- Myra-0.0.1/src/com/likya/myra/jef/ConfigurationManagerBean.java
+++ Myra-0.0.1/src/com/likya/myra/jef/ConfigurationManagerBean.java
@@ -15,20 +15,46 @@
  ******************************************************************************/
 package com.likya.myra.jef;
 
+import org.apache.xmlbeans.XmlException;
+
 import com.likya.myra.jef.model.JobRuntimeProperties;
 import com.likya.myra.jef.model.TemporaryConfig;
+import com.likya.myra.jef.utils.FileUtils;
+import com.likya.xsd.myra.model.xbeans.stateinfo.GlobalStateDefinitionDocument;
+import com.likya.xsd.myra.model.xbeans.stateinfo.GlobalStateDefinitionDocument.GlobalStateDefinition;
 
 public class ConfigurationManagerBean implements ConfigurationManager {
 
+	private TemporaryConfig temporaryConfig;
+	private JobRuntimeProperties jobRuntimeProperties;
+	private GlobalStateDefinition globalStateDefinition;
+	
+	public ConfigurationManagerBean() {
+		super();
+		this.temporaryConfig = new TemporaryConfig();	
+		this.jobRuntimeProperties = new JobRuntimeProperties();
+		
+		StringBuffer xmlString = FileUtils.readFile("globalStates.xml");
+		try {
+			GlobalStateDefinitionDocument globalStateDefinitionDocument = GlobalStateDefinitionDocument.Factory.parse(xmlString.toString());
+			this.globalStateDefinition = globalStateDefinitionDocument.getGlobalStateDefinition();
+		} catch (XmlException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@Override
 	public TemporaryConfig getTemporaryConfig() {
-		return new TemporaryConfig();		
+		return temporaryConfig;
 	}
 
 	@Override
 	public JobRuntimeProperties getJobRuntimeProperties() {
-		JobRuntimeProperties jobRuntimeProperties = new JobRuntimeProperties();
 		return jobRuntimeProperties;
+	}
+
+	public GlobalStateDefinition getGlobalStateDefinition() {
+		return globalStateDefinition;
 	}
 
 }
