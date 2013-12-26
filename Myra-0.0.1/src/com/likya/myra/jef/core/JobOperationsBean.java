058078f89ad2872036e5020c2d66361155ba97b5
hulagu <serkan.tas@likyateknoloji.com>
2013-12-26 16:59:02 +0200

    .

--- Myra-0.0.1/src/com/likya/myra/jef/core/JobOperationsBean.java
+++ Myra-0.0.1/src/com/likya/myra/jef/core/JobOperationsBean.java
@@ -15,7 +15,28 @@
  ******************************************************************************/
 package com.likya.myra.jef.core;
 
+import com.likya.myra.LocaleMessages;
+import com.likya.myra.jef.OutputStrategy;
+import com.likya.myra.jef.jobs.JobHelper;
+import com.likya.myra.jef.model.OutputData;
+import com.likya.xsd.myra.model.joblist.AbstractJobType;
+import com.likya.xsd.myra.model.stateinfo.StateNameDocument.StateName;
+import com.likya.xsd.myra.model.stateinfo.StatusNameDocument.StatusName;
+import com.likya.xsd.myra.model.stateinfo.SubstateNameDocument.SubstateName;
+
 public class JobOperationsBean implements JobOperations {
+	
+	private CoreFactory coreFactory;
+	
+	public JobOperationsBean(CoreFactory coreFactory) {
+		super();
+		this.coreFactory = coreFactory;
+	}
+
+	protected void sendOutputData(Object object) {
+		OutputStrategy outputStrategy = coreFactory.getOutputStrategy();
+		outputStrategy.sendDataObject(object);
+	}
 
 	@Override
 	public void retryExecution(String jobName) {
@@ -67,8 +88,26 @@ public class JobOperationsBean implements JobOperations {
 
 	@Override
 	public void disableJob(String jobName) {
-		// TODO Auto-generated method stub
 		
+		CoreFactory.getLogger().info(LocaleMessages.getString("TlosCommInterface.35") + jobName);
+
+		if (coreFactory.getMonitoringOperations().getJobQueue().containsKey(jobName)) {
+
+			AbstractJobType abstractJobType = coreFactory.getMonitoringOperations().getJobQueue().get(jobName).getAbstractJobType();
+			
+			JobHelper.insertNewLiveStateInfo(abstractJobType, StateName.INT_CANCELLED, SubstateName.INT_DEACTIVATED, StatusName.INT_BYUSER);
+			
+			OutputData outputData = OutputData.generateDefault(abstractJobType);
+			
+			sendOutputData(outputData);
+
+//			TODO yeni yapıda bu iş nasıl olacak ?
+//			synchronized (TlosServer.getDisabledJobQueue()) {
+//				TlosServer.getDisabledJobQueue().put(jobName, jobName);
+//			}
+			
+			CoreFactory.getLogger().info(LocaleMessages.getString("TlosCommInterface.19") + jobName + " : " + abstractJobType.getStateInfos().getLiveStateInfos().getLiveStateInfoArray(0).toString());
+		}
 	}
 
 	@Override
