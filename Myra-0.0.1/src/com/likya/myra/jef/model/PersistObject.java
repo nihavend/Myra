f30599a06584ab9cc28cd936c330a1d39639bf74
hulagu <serkan.tas@likyateknoloji.com>
2013-12-31 13:19:41 +0200

    .

--- Myra-0.0.1/src/com/likya/myra/jef/model/PersistObject.java
+++ Myra-0.0.1/src/com/likya/myra/jef/model/PersistObject.java
@@ -1,9 +1,18 @@
-/*
- * Tlos_V1.61
- * com.likya.tlos.model : PersistObject.java
- * @author Serkan Taş
- * Tarih : 12.Ara.2010 19:27:50
- */
+/*******************************************************************************
+ * Copyright 2013 Likya Teknoloji
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 
 package com.likya.myra.jef.model;
 
