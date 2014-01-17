8f07728c8c2f36107daf2f9b7c82be3118ead65e
hulagu <serkan.tas@likyateknoloji.com>
2014-01-17 11:08:53 +0200

    .

--- Myra-0.0.1/src/com/likya/myra/jef/utils/Scheduler.java
+++ Myra-0.0.1/src/com/likya/myra/jef/utils/Scheduler.java
@@ -49,7 +49,7 @@ public class Scheduler {
 
 		if (nextPeriodTime == null) {
 			Calendar selectedSchedule = regularSchedule(abstractJobType);
-			if (selectedSchedule != null && selectedSchedule.after(Calendar.getInstance()) && !selectedSchedule.after(abstractJobType.getManagement().getTimeManagement().getJsPlannedTime().getStopTime())) {
+			if (selectedSchedule != null && selectedSchedule.after(Calendar.getInstance())) {
 				abstractJobType.getManagement().getTimeManagement().getJsPlannedTime().setStartTime(selectedSchedule);
 				// yeni zamana kuruldu
 			} else {
@@ -63,7 +63,7 @@ public class Scheduler {
 
 	private static Calendar regularSchedule(AbstractJobType abstractJobType) {
 
-		Calendar tmpCal = abstractJobType.getManagement().getTimeManagement().getJsPlannedTime().getStartTime();
+		Calendar jsPlannedStartTime = abstractJobType.getManagement().getTimeManagement().getJsPlannedTime().getStartTime();
 
 		Calendar selectedSchedule = null;
 
@@ -71,7 +71,7 @@ public class Scheduler {
 
 		int daysOfWeek[] = scheduleInfo.getDaysOfWeekIntTypeArray();
 
-		RestrictedDailyIterator restrictedDailyIterator = new RestrictedDailyIterator(tmpCal.get(Calendar.HOUR_OF_DAY), tmpCal.get(Calendar.MINUTE), tmpCal.get(Calendar.SECOND), daysOfWeek);
+		RestrictedDailyIterator restrictedDailyIterator = new RestrictedDailyIterator(jsPlannedStartTime.get(Calendar.HOUR_OF_DAY), jsPlannedStartTime.get(Calendar.MINUTE), jsPlannedStartTime.get(Calendar.SECOND), daysOfWeek);
 		Calendar restCal = restrictedDailyIterator.next();
 		selectedSchedule = restCal;
 
@@ -81,26 +81,24 @@ public class Scheduler {
 			
 			int dayList[] = daysOfMonth.getDaysArray();
 
-			if (Arrays.binarySearch(dayList, tmpCal.get(Calendar.DAY_OF_MONTH)) > 0) {
-				if (tmpCal.before(selectedSchedule)) {
-					selectedSchedule = tmpCal;
-				}
-			}
-
 			String firstDay = daysOfMonth.getFirstDayOfMonth();
-
-			if (firstDay != null && !firstDay.equals("")) {
+			
+			String lastDay = daysOfMonth.getLastDayOfMonth();
+			
+			if (Arrays.binarySearch(dayList, Calendar.DAY_OF_MONTH) > 0) {
+				if (jsPlannedStartTime.before(selectedSchedule)) {
+					// System.err.println("selectedSchedule : " + MyraDateUtils.getDate(selectedSchedule.getTime()));
+					// System.err.println("jsPlannedStartTime : " + MyraDateUtils.getDate(jsPlannedStartTime.getTime()));
+					selectedSchedule = jsPlannedStartTime;
+				}
+			} else if (firstDay != null && !firstDay.equals("")) {
 				Calendar localCal = Calendar.getInstance();
 				int lastDayOfMonth = localCal.getActualMaximum(Calendar.DAY_OF_MONTH);
 				localCal.set(Calendar.DAY_OF_MONTH, lastDayOfMonth);
 				if (localCal.before(selectedSchedule)) {
 					selectedSchedule = localCal;
 				}
-			}
-
-			String lastDay = daysOfMonth.getLastDayOfMonth();
-
-			if (lastDay != null && !lastDay.equals("")) {
+			} if (lastDay != null && !lastDay.equals("")) {
 				Calendar localCal = Calendar.getInstance();
 				int lastDayOfMonth = localCal.getActualMinimum(Calendar.DAY_OF_MONTH);
 				localCal.set(Calendar.DAY_OF_MONTH, lastDayOfMonth);
