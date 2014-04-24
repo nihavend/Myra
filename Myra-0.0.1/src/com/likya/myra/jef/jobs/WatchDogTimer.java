ea67a990c00d618a7f51018fb673c6a729e2fe6e
hulagu <serkan.tas@likyateknoloji.com>
2014-04-24 16:50:42 +0300

    comments organized

--- Myra-0.0.1/src/com/likya/myra/jef/jobs/WatchDogTimer.java
+++ Myra-0.0.1/src/com/likya/myra/jef/jobs/WatchDogTimer.java
@@ -21,6 +21,33 @@ import com.likya.xsd.myra.model.stateinfo.StateNameDocument.StateName;
 import com.likya.xsd.myra.model.stateinfo.StatusNameDocument.StatusName;
 import com.likya.xsd.myra.model.stateinfo.SubstateNameDocument.SubstateName;
 
+/**
+ * @author serkan Taş 18.03.2011 Daha önce : 1. isAutoRetry == true ise, ilk
+ *         time-out olduğunda kill edip yeniden çalıştırmayı deniyorduk 2.
+ *         isAutoRetry == false ise, ilk time-out sonrası mesajlar
+ *         gönderiliyor, bir time-out süresi daha bekleyip işlemi stop edip
+ *         statüyü fail yapıyorduk.
+ * 
+ *         (18:03.2011) : 1. isAutoRetry == true ise, ilk time-out olduğunda
+ *         kill edip yeniden çalıştırmayı deniyoruz
+ * 
+ *         2. isAutoRetry == false ise, ilk time-out sonrası mesajlar
+ *         gönderiliyor, kullanıcı stop edene kadar time-out da kalıyor.
+ * 
+ * 
+ * @author merve 11.02.2013
+ * 
+ *         İş bir kere time-out'a düştükten sonra bir daha time-out süresini
+ *         kontrol etmiyoruz.
+ * 
+ *         1. isAutoRetry == true ise, ilk time-out olduğunda kill edip
+ *         yeniden çalıştırmayı deniyoruz.
+ * 
+ *         2. isAutoRetry == false ise, ilk time-out olduğunda işi time-out
+ *         statüsüne çekip uyarı mesajlarını gönderiyoruz. İş timeout
+ *         statüsünde çalışmaya devam ediyor.
+ */
+
 public class WatchDogTimer extends Thread {
 
 
@@ -36,33 +63,6 @@ public class WatchDogTimer extends Thread {
 		// Not 1 den dolayı gerek kalmadı
 		this.jobImpl = jobImpl;
 	}
-
-	/**
-	 * @author serkan Taş 18.03.2011 Daha önce : 1. isAutoRetry == true ise, ilk
-	 *         time-out olduğunda kill edip yeniden çalıştırmayı deniyorduk 2.
-	 *         isAutoRetry == false ise, ilk time-out sonrası mesajlar
-	 *         gönderiliyor, bir time-out süresi daha bekleyip işlemi stop edip
-	 *         statüyü fail yapıyorduk.
-	 * 
-	 *         (18:03.2011) : 1. isAutoRetry == true ise, ilk time-out olduğunda
-	 *         kill edip yeniden çalıştırmayı deniyoruz
-	 * 
-	 *         2. isAutoRetry == false ise, ilk time-out sonrası mesajlar
-	 *         gönderiliyor, kullanıcı stop edene kadar time-out da kalıyor.
-	 * 
-	 * 
-	 * @author merve 11.02.2013
-	 * 
-	 *         İş bir kere time-out'a düştükten sonra bir daha time-out süresini
-	 *         kontrol etmiyoruz.
-	 * 
-	 *         1. isAutoRetry == true ise, ilk time-out olduğunda kill edip
-	 *         yeniden çalıştırmayı deniyoruz.
-	 * 
-	 *         2. isAutoRetry == false ise, ilk time-out olduğunda işi time-out
-	 *         statüsüne çekip uyarı mesajlarını gönderiyoruz. İş timeout
-	 *         statüsünde çalışmaya devam ediyor.
-	 */
 
 	public void run() {
 		try {
