2174e61baa2dc7984be8a9492e938b12277601d8
hulagu <serkan.tas@likyateknoloji.com>
2015-01-03 22:30:57 +0200

    .

---
+++ Myra-0.0.1/src/com/likya/myra/jef/utils/Starter.java
@@ -0,0 +1,97 @@
+package com.likya.myra.jef.utils;
+
+import org.apache.log4j.Logger;
+
+import com.likya.commons.utils.FileUtils;
+import com.likya.myra.commons.utils.XMLValidations;
+import com.likya.myra.jef.InputStrategy;
+import com.likya.myra.jef.InputStrategyImpl;
+import com.likya.myra.jef.OutputStrategy;
+import com.likya.myra.jef.core.CoreFactory;
+import com.likya.myra.jef.core.ManagementOperations;
+import com.likya.xsd.myra.model.joblist.JobListDocument;
+
+public class Starter {
+	
+	public static CoreFactory startForce(String senaryo, OutputStrategy outputStrategy) throws Exception {
+		
+		CoreFactory coreFactory;
+		JobListDocument jobListDocument;
+		
+		StringBuffer xmlString = getData(senaryo);
+		
+		if(xmlString == null) {
+			jobListDocument = JobListDocument.Factory.newInstance();
+			jobListDocument.addNewJobList();
+		} else {
+			jobListDocument = JobListDocument.Factory.parse(xmlString.toString());
+		}
+		
+		coreFactory = doIt(jobListDocument, outputStrategy);
+		
+		return coreFactory;
+	}
+
+	public static CoreFactory start(OutputStrategy outputStrategy) throws Exception {
+		
+		JobListDocument jobListDocument = JobListDocument.Factory.newInstance();
+
+		jobListDocument.addNewJobList();
+		
+		CoreFactory coreFactory = doIt(jobListDocument, outputStrategy);
+		
+		return coreFactory;
+	}
+	
+	public static CoreFactory start(String senaryo, OutputStrategy outputStrategy) throws Exception {
+		
+		StringBuffer xmlString = getData(senaryo);
+
+		JobListDocument jobListDocument = JobListDocument.Factory.parse(xmlString.toString());
+
+		CoreFactory coreFactory = doIt(jobListDocument, outputStrategy);
+		
+		return coreFactory;
+	}
+	
+	private static CoreFactory doIt(JobListDocument jobListDocument, OutputStrategy outputStrategy ) {
+		
+		InputStrategy inputStrategy = new InputStrategyImpl();
+
+		inputStrategy.setJobListDocument(jobListDocument);
+
+		CoreFactory coreFactory = (CoreFactory) CoreFactory.getInstance(inputStrategy, outputStrategy);
+
+		ManagementOperations managementOperations = coreFactory.getManagementOperations();
+		
+		try {
+			managementOperations.start();
+		} catch (Throwable e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		return coreFactory;
+	}
+	
+	public static StringBuffer getData(String senaryo) throws Exception {
+
+		StringBuffer xmlString;
+
+		if (senaryo == null || FileUtils.checkFile(senaryo) == false) {
+			// throw new Exception(senaryo + " is null, damaged or not exists at all !");
+			CoreFactory.getLogger().debug(senaryo + " is null, damaged or not exists at all !");
+			return null;
+		}
+
+		xmlString = FileUtils.readFile(senaryo);
+
+		JobListDocument jobListDocument = JobListDocument.Factory.parse(xmlString.toString());
+
+		if (!XMLValidations.validateWithXSDAndLog(Logger.getRootLogger(), jobListDocument)) {
+			throw new Exception(senaryo + " is null or damaged !");
+		}
+
+		return xmlString;
+	}
+}
