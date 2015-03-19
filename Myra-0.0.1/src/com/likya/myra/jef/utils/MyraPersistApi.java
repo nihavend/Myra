da76f1a922cd1d9fbb093230bdab2ffe557baafe
hulagu <serkan.tas@likyateknoloji.com>
2015-03-19 17:06:19 +0200

    .

--- Myra-0.0.1/src/com/likya/myra/jef/utils/MyraPersistApi.java
+++ Myra-0.0.1/src/com/likya/myra/jef/utils/MyraPersistApi.java
@@ -10,22 +10,156 @@ import java.io.ObjectOutputStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.apache.log4j.Logger;
+
 import com.likya.commons.utils.FileUtils;
 import com.likya.myra.commons.utils.LiveStateInfoUtils;
+import com.likya.myra.commons.utils.XMLValidations;
 import com.likya.myra.jef.ConfigurationManager;
 import com.likya.myra.jef.core.CoreFactory;
 import com.likya.myra.jef.jobs.JobImpl;
 import com.likya.myra.jef.model.PersistObject;
 import com.likya.xsd.myra.model.config.MyraConfigDocument;
+import com.likya.xsd.myra.model.config.MyraConfigDocument.MyraConfig;
+import com.likya.xsd.myra.model.config.UsejobnamesforlogDocument.Usejobnamesforlog;
 import com.likya.xsd.myra.model.stateinfo.StateNameDocument.StateName;
 import com.likya.xsd.myra.model.stateinfo.StatusNameDocument.StatusName;
 import com.likya.xsd.myra.model.stateinfo.SubstateNameDocument.SubstateName;
 
 public class MyraPersistApi {
+
+	private static boolean persistent = false;
+	private static boolean normalize = false;
+	private static int frequency = 1;
+	private static int higherThreshold = 10;
+	private static int lowerThreshold = 3;
+	private static String logPath = "logs";
+	
+	private static String logFileExt = ".log";
+	private static String globalLogPath = "logs";
+	private static int logbuffersize = 800;
+	private static int logpagesize = 10;
+	
+	public static MyraConfig setDefaults() {
+
+		MyraConfigDocument myraConfigDocument = MyraConfigDocument.Factory.newInstance();
+		myraConfigDocument.addNewMyraConfig();
+		
+		MyraConfig myraConfig = myraConfigDocument.getMyraConfig();
+		myraConfig.setPersistent(persistent);
+		myraConfig.setNormalize(normalize);
+		myraConfig.setFrequency((short) frequency);
+		myraConfig.setHigherThreshold((short) higherThreshold);
+		myraConfig.setLowerThreshold((short) lowerThreshold);
+		myraConfig.setLogPath(logPath);
+		myraConfig.setUsejobnamesforlog(Usejobnamesforlog.NO);
+		myraConfig.setLogFileExt(logFileExt);
+		myraConfig.setGlobalLogPath(globalLogPath);
+		myraConfig.setLogbuffersize((short) logbuffersize);
+		myraConfig.setLogpagesize((short) logpagesize);
+
+		return myraConfig;
+	}
+	
+	public static void checkDataPath() {
+		
+		Path dataPath = Paths.get(CoreFactory.MYRA_DATA_PATH);
+		
+		if (Files.notExists(dataPath)) {
+			try {
+				Files.createDirectory(dataPath);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(-1);
+			}
+		}
+		
+	}
+	
+	public static boolean checkMyraConfig() {
+		
+		boolean retValue = false;
+		
+		Path configPath = Paths.get(CoreFactory.CONFIG_PATH);
+
+		if (Files.notExists(configPath)) {
+			try {
+				Files.createDirectory(configPath);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(-1);
+			}
+		}
+
+		Path configFile = Paths.get(CoreFactory.CONFIG_PATH + File.separator + CoreFactory.CONFIG_FILE);
+		if (Files.exists(configFile)) {
+			retValue = true;
+		}
+		
+		return retValue;
+	}
+	
+	public static MyraConfig getMyraConfig(boolean initialize) {
+
+		boolean retValue = checkMyraConfig();
+		
+		MyraConfig myraConfig = null;
+		
+		Path configFile = Paths.get(CoreFactory.CONFIG_PATH + File.separator + CoreFactory.CONFIG_FILE);
+		
+		if(retValue) {
+			myraConfig = readConfig(configFile).getMyraConfig();
+		} else {
+			myraConfig = setDefaults();
+			if(initialize) {
+				createSerializeConfig(configFile.toString(), myraConfig);
+			}
+		}
+
+		return myraConfig;
+	}
+	
+	public static MyraConfigDocument readConfig(Path configFile) {
+
+		MyraConfigDocument myraConfigDocument = null;
+
+		String myraConfigFile = configFile.toString();
+
+		StringBuffer xmlString = FileUtils.readFile(myraConfigFile);
+
+		//		ConfigurationManager configurationManager;
+
+		try {
+			myraConfigDocument = MyraConfigDocument.Factory.parse(xmlString.toString());
+
+			if (!XMLValidations.validateWithXSDAndLog(Logger.getRootLogger(), myraConfigDocument)) {
+				throw new Exception("myraConfigDocument is null or damaged !");
+			}
+
+			//			configurationManager = new ConfigurationManagerImpl(myraConfigDocument);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return myraConfigDocument;
+	}
+	
+	public static void createSerializeConfig(String fileName, MyraConfig myraConfig) {
+		MyraConfigDocument myraConfigDocument = MyraConfigDocument.Factory.newInstance();
+		myraConfigDocument.addNewMyraConfig().set(myraConfig);
+		try {
+			MyraPersistApi.serializeConfig(fileName, myraConfigDocument);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
 
 	public static void serializeConfig(String fileName, MyraConfigDocument myraConfigDocument) throws Exception {
 
