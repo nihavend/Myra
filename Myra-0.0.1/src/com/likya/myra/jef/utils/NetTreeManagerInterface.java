904d43c0cbed97a9599a0166b799cebf4007dafc
hulagu <serkan.tas@likyateknoloji.com>
2015-04-20 18:12:50 +0300

    .

--- Myra-0.0.1/src/com/likya/myra/jef/utils/NetTreeManagerInterface.java
+++ Myra-0.0.1/src/com/likya/myra/jef/utils/NetTreeManagerInterface.java
@@ -4,13 +4,12 @@ import java.util.HashMap;
 
 import com.likya.myra.commons.utils.NetTreeResolver.NetTree;
 import com.likya.myra.jef.utils.NetTreeManagerImpl.NetTreeMonitor;
-import com.likya.xsd.myra.model.joblist.AbstractJobType;
 
 public interface NetTreeManagerInterface {
 
 	public HashMap<String, NetTree> getNetTreeMap();
 
-	public HashMap<String, AbstractJobType> getFreeJobs();
+	public HashMap<String, String> getFreeJobs();
 	
 	public HashMap<String, NetTreeMonitor> getNetTreeMonitorMap();
 		
